a51141f04bd0d6a1291563c0e38a34fbbbba17a0
Zoia611 <117438266+Zoia611@users.noreply.github.com>
2022-11-23 15:24:26 +0300

    Добавьте файлы проекта.

---
+++ Task1/Function.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    struct Function
+    {
+       
+        double k;
+        double b;
+
+        public Function(double k, double b)
+        {
+            this.k = k;
+            this.b = b;
+        }
+        public string Root()
+        {
+            if (k == 0 && b == 0)
+            {
+                return "любое число";
+            }
+            else if (b == 0)
+            {
+                return "0";
+            }
+            else
+            {
+                return $"{-b / k}";
+            }
+        }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        struct Function
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("В уравнении 0 = k*x -b, задайте коэффициенты k и b");
+                double k = Convert.ToDouble(Console.ReadLine());
+                double b = Convert.ToDouble(Console.ReadLine());
+                Function function = new Function(k, b);
+                Console.WriteLine("Решение: {0}", function.Root());
+                Console.ReadKey();
+            }
+        }
+        
+    }
+}
